413faa3126a80dce276957953fc19b7edf49aa3a
Vitor Ginez <vitorginez2018@gmail.com>
2026-05-23 23:23:10 -0300

    Add files via upload

---
+++ Baixar videos com python/main.py
@@ -0,0 +1,8 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress 
+
+url = input("Coloque o URL do vídeo do YouTube: ")
+yt = YouTube(url, on_progress_callback=on_progress)
+yt.streams.get_highest_resolution().download(output_path="C:/Users/jotag/Downloads")
+
+
