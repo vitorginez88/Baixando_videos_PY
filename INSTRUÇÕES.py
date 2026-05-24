a527d5de4e1c73368ca8855ea7480bc1b86a9d22
Vitor Ginez <vitorginez2018@gmail.com>
2026-05-24 20:26:43 -0300

    Revise download instructions and add error handling
    Updated instructions and error handling for YouTube downloader.

--- INSTRUÇÕES.py
+++ INSTRUÇÕES.py
@@ -7,11 +7,17 @@ from pytubefix import YouTube
 #coloca uma barra de progresso do download:
 from pytubefix.cli import on_progress 
 
-#codigo para pegar a url do vídeo e colocar na pasta downloads 
+#codigo para pegar a url do vídeo e colocar na pasta downloads:
 url = input("Coloque o URL do vídeo do YouTube: ")
+#Escolhe a maior resolução disponível e baixa o vídeo:
 yt = YouTube(url, on_progress_callback=on_progress)
+
 #IMPORTANTE: 
-#Quando colar o código mude "USUARIO" pelo usuário que esta logado no seu computador.
-yt.streams.get_highest_resolution().download(output_path="/Users/USUARIO/Downloads")
+#Quando colar o código mude "COLOCAR_DESTINO" por onde você vai querer salvar o vídeo.
+yt.streams.get_highest_resolution().download(output_path="COLOCAR_DESTINO")
 
 print("Download concluído!")
+
+#faz com que programa não feche caso o link esteja errado:
+except Exception as erro:
+    print("Ocorreu um erro:", erro)
