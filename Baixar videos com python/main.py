2d0eaab11d26af1e1c6e5856d295da0344b491d2
Vitor Ginez <vitorginez2018@gmail.com>
2026-05-23 23:23:57 -0300

    Update main.py

--- Baixar videos com python/main.py
+++ Baixar videos com python/main.py
@@ -3,6 +3,6 @@ from pytubefix.cli import on_progress
 
 url = input("Coloque o URL do vídeo do YouTube: ")
 yt = YouTube(url, on_progress_callback=on_progress)
-yt.streams.get_highest_resolution().download(output_path="C:/Users/jotag/Downloads")
+yt.streams.get_highest_resolution().download(output_path="C:/Users/USUARIO/Downloads")
 
 
