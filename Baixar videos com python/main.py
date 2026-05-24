cb795153dbb696490672a831e725f8bd276f0606
Vitor Ginez <vitorginez2018@gmail.com>
2026-05-23 23:28:11 -0300

    Delete Baixar videos com python/main.py

--- Baixar videos com python/main.py
+++
@@ -1,8 +0,0 @@
-from pytubefix import YouTube
-from pytubefix.cli import on_progress 
-
-url = input("Coloque o URL do vídeo do YouTube: ")
-yt = YouTube(url, on_progress_callback=on_progress)
-yt.streams.get_highest_resolution().download(output_path="C:/Users/USUARIO/Downloads")
-
-
