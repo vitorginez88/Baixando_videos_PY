553a0b9027cdd8f96093857376829419b9c8af09
Vitor Ginez <vitorginez2018@gmail.com>
2026-05-23 23:39:34 -0300

    Revise comments and instructions in INSTRUÇÕES.py
    Updated comments for clarity and added instructions for user customization.

--- INSTRUÇÕES.py
+++ INSTRUÇÕES.py
@@ -1,11 +1,17 @@
-#primeiro passo é baixa a bilbioteca pytubefix no seu terminal utilizando o "pip install pytubefix"
-#para que o código funcione.
+#IMPORTANTE 
+#Para que o código funcione o primeiro passo é baixar a bilbioteca pytubefix no seu terminal
+#Utilizando o comando "pip install pytubefix"
 
 #esse código importa a biblioteca.
 from pytubefix import YouTube
 #coloca uma barra de progresso do download:
 from pytubefix.cli import on_progress 
 
+#codigo para pegar a url do vídeo e colocar na pasta downloads 
 url = input("Coloque o URL do vídeo do YouTube: ")
 yt = YouTube(url, on_progress_callback=on_progress)
+#IMPORTANTE: 
+#Quando colar o código mude "USUARIO" pelo usuário que esta logado no seu computador.
 yt.streams.get_highest_resolution().download(output_path="C:/Users/USUARIO/Downloads")
+
+print("Download concluído!")
